fa8a4025393d394820979b384ae74a6394b37a53
CrioUser <crio-user@criodo.com>
2022-03-23 01:38:47 +0530

    “ME_QKART_FRONTEND_V2_MODULE_REGISTER”

--- src/components/Register.js
+++ src/components/Register.js
@@ -7,7 +7,11 @@ import { config } from "../App";
 import Footer from "./Footer";
 import Header from "./Header";
 import "./Register.css";
+import { Link , useHistory} from "react-router-dom";
 
+
+// const POST_API =  `${config.endpoint}/auth/register`
+// console.log(POST_API)
 const Register = () => {
   const { enqueueSnackbar } = useSnackbar();
 
@@ -35,7 +39,50 @@ const Register = () => {
    *      "message": "Username is already taken"
    * }
    */
+
+   const userData = {
+     username : "",
+     password : "",
+     confirmPassword : ""
+   };
+
+   const [registeredData , setRegisteredData] = useState(userData);
+
+   const handleData = (event) =>{
+    const name = event.target.name;
+    const value = event.target.value;
+    setRegisteredData({...registeredData, [name] : value})
+   }
+
+   const [isLoading , setIsLoading] = useState(false)
+
+   /***login */
+       const history = useHistory();
+
   const register = async (formData) => {
+       const collectFormData = {...formData}
+        try{
+          setIsLoading(true)
+       const res = await axios.post(`${config.endpoint}/auth/register` ,collectFormData);
+       const data = res.data;
+          setIsLoading(false)
+        console.log(data);
+       enqueueSnackbar('Registered Successfully',{ 
+        variant: 'success',
+        })
+              history.push("/login")
+      }
+        catch(e){
+          setIsLoading(false)
+          if (e.response && e.response.status === 400)
+          {
+              return  enqueueSnackbar( e.response.data.message, { variant: 'error'});
+         }
+         else {
+                 enqueueSnackbar( "Something went wrong. Check that the backend is running, reachable and returns valid JSON.", { variant: 'error'});
+         }
+      }
+     
   };
 
   // TODO: CRIO_TASK_MODULE_REGISTER - Implement user input validation logic
@@ -56,61 +103,117 @@ const Register = () => {
    * -    Check that password field is not less than 6 characters in length - "Password must be at least 6 characters"
    * -    Check that confirmPassword field has the same value as password field - Passwords do not match
    */
-  const validateInput = (data) => {
-  };
 
-  return (
-    <Box
-      display="flex"
-      flexDirection="column"
-      justifyContent="space-between"
-      minHeight="100vh"
-    >
-      <Header hasHiddenAuthButtons />
-      <Box className="content">
-        <Stack spacing={2} className="form">
-          <h2 className="title">Register</h2>
-          <TextField
-            id="username"
-            label="Username"
-            variant="outlined"
-            title="Username"
-            name="username"
-            placeholder="Enter Username"
-            fullWidth
-          />
-          <TextField
-            id="password"
-            variant="outlined"
-            label="Password"
-            name="password"
-            type="password"
-            helperText="Password must be atleast 6 characters length"
-            fullWidth
-            placeholder="Enter a password with minimum 6 characters"
-          />
-          <TextField
-            id="confirmPassword"
-            variant="outlined"
-            label="Confirm Password"
-            name="confirmPassword"
-            type="password"
-            fullWidth
-          />
-           <Button className="button" variant="contained">
-            Register Now
-           </Button>
-          <p className="secondary-action">
-            Already have an account?{" "}
-             <a className="link" href="#">
-              Login here
-             </a>
-          </p>
-        </Stack>
-      </Box>
-      <Footer />
-    </Box>
-  );
+  const validateInput = (data) => {
+    
+    let validData = {...data}
+
+    if(!validData.username)
+    {
+      enqueueSnackbar("Username is a required field",{ 
+        variant: 'warning',
+    })
+       return false
+  }
+ 
+     if(validData.username.length < 6)
+     {
+       enqueueSnackbar("Username must be at least 6 characters",{ 
+            variant: 'warning',
+       })
+        return false
+    }
+
+    if(!validData.password)
+    {
+      enqueueSnackbar("Password is a required field",{ 
+        variant: 'warning',
+    })
+         return false
+  }
+ 
+       if(validData.password.length < 6)
+     {
+       enqueueSnackbar("Password must be at least 6 characters",{ 
+            variant: 'warning',
+       })
+         return false
+    }
+    
+    if(validData.password !==validData.confirmPassword)
+    {
+      enqueueSnackbar("Passwords do not match",{ 
+           variant: 'warning',
+      })
+          return false
+   }
+     delete validData.confirmPassword;
+     return register(validData) ;
+   
+  };
+      
+   
+
+      return (
+        <Box
+          display="flex"
+          flexDirection="column"
+          justifyContent="space-between"
+          minHeight="100vh"
+        >
+          <Header hasHiddenAuthButtons ={false} />
+          <Box className="content">
+            <Stack spacing={2} className="form">
+              <h2 className="title">Register</h2>
+              <TextField
+                id="username"
+                label="Username"
+                variant="outlined"
+                title="Username"
+                name="username"
+                value = {registeredData.username}
+                onChange = {handleData}
+                placeholder="Enter Username"
+                fullWidth
+              />
+              <TextField
+                id="password"
+                variant="outlined"
+                label="Password"
+                name="password"
+                type="password"
+                value = {registeredData.password}
+                onChange = {handleData}
+                helperText="Password must be atleast 6 characters length"
+                fullWidth
+              />
+              <TextField
+                id="confirmPassword"
+                variant="outlined"
+                label="Confirm Password"
+                name="confirmPassword"
+                value = {registeredData.confirmPassword}
+                onChange = {handleData}
+                type="password"
+                fullWidth
+              />
+               {isLoading ? <Box  display="flex"  justifyContent="center">
+                               <CircularProgress color="success"/>
+                            </Box>  
+                     : <Button className="button" variant="contained" onClick ={() => validateInput(registeredData)}>
+                Register Now
+              </Button>}
+              <p className="secondary-action">
+                Already have an account?{" "}
+                <Link className="link" to="/login">
+                  Login here
+                </Link>
+              </p>
+            </Stack>
+          </Box>
+          <Footer />
+        </Box>
+      );
 };
 
 export default Register;
